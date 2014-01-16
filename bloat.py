37a31fc149f871c242e25dd37df2cd1388ea3e31
Darren Garvey <darren.garvey@youview.com>
2014-01-16 00:52:04 +0000

    Support for Python < 2.7.

--- bloat.py
+++ bloat.py
@@ -24,6 +24,23 @@ import subprocess
 import sys
 import json
 
+###################### HACK #####################
+if "check_output" not in dir( subprocess ): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd)
+        return output
+    subprocess.check_output = f
+###################### HACK #####################
+
 def format_bytes(bytes):
     """Pretty-print a number of bytes."""
     if bytes > 1e6:
